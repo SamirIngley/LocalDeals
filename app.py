cd8bb0cb229d57a5faaa94029b73c346adefe049
SamirIngley <samir.ingle7@gmail.com>
2020-03-20 01:36:01 -0700

    websites successfully extracted

--- app.py
+++ app.py
@@ -21,6 +21,19 @@ def google():
     # Define Search
     places_result = gmaps.places_nearby(location='-33.8670522,151.1957362', radius=10000, open_now=False, type="cafe")
     pprint.pprint(places_result)
+
+    # Get the next 20 results - pause it for 3 seconds. 
+    # time.sleep(3)
+    # places_result2 = gmaps.places_nearby(page_token = places_result['next_page_token'])
+
+    # loop through each place in results, extract website
+    for place in places_result['results']:
+        my_place_id = place['place_id']
+        my_fields = ['name', 'website', 'type']
+
+        place_details = gmaps.place(my_place_id, fields = my_fields)
+        print(place_details)
+
     return 'hello google'
 
 
