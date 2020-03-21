98fb64b1479556acc390b6f4d4672bac4420106e
SamirIngley <samir.ingle7@gmail.com>
2020-03-20 22:52:08 -0700

    got individual websites

--- googleapi.py
+++ googleapi.py
@@ -18,7 +18,7 @@ def googled():
 
     # Define Search
     places_result = gmaps.places_nearby(location='-33.8670522,151.1957362', radius=10000, open_now=False, type="cafe")
-    pprint.pprint(places_result)
+    # pprint.pprint(places_result)
 
     # Get the next 20 results - pause it for 3 seconds or else won't work 
     # time.sleep(3)
@@ -26,11 +26,26 @@ def googled():
 
     # loop through each place in results, extract website
 
+    details_list = []
+    website_list = []
+
     for place in places_result['results']:
         my_place_id = place['place_id']
         my_fields = ['name', 'website', 'type']
 
         place_details = gmaps.place(my_place_id, fields = my_fields)
-        print(place_details)
+        details_list += (place_details)
+        print('DETAILS ', place_details)
+        print('RESULT ', place_details['result'])
+        # print(place_details['result'].keys())
+        if 'website' in place_details['result'].keys():
+            print('WEBSITE ', place_details['result']['website'])
+            website = place_details['result']['website']
+            website_list.append(website)
 
-    return 'Google' 
+    counter = 1
+    for item in website_list:
+        print(counter, item)
+        counter += 1
+
+    return 'hello'
