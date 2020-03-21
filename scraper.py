274d375b18b7961c10a70fdb54c12c14602621bc
SamirIngley <samir.ingle7@gmail.com>
2020-03-21 02:31:53 -0700

    scraper works for ul but not text

--- scraper.py
+++ scraper.py
@@ -1,14 +1,47 @@
 import requests
+from bs4 import BeautifulSoup as bs
 
-url = 'http://geo.craigslist.org/iso/us/'
 
-# user agent: what browser and what os we are using
+url = 'https://www.macys.com/'
 headers = {'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/80.0.3987.95 Mobile/15E148 Safari/605.1'}
-
 response = requests.get(url, headers=headers)
 
-# print(response.status_code)
-# print(response.headers)
+print(response.status_code)
 
-# WEBSITE DATA: CONTENT  !!!!!!!
-print(response.content)
+soup = bs(response.content, 'html.parser')
+# print(soup.prettify)
+
+sales = soup.find_all('ul')
+print(sales)
+
+
+# url = 'http://geo.craigslist.org/iso/us/'
+
+# # user agent: what browser and what os we are using
+# headers = {'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/80.0.3987.95 Mobile/15E148 Safari/605.1'}
+
+# response = requests.get(url, headers=headers)
+
+# # Make sure it works
+# # print(response.status_code)
+
+# # print(response.headers)
+
+# # WEBSITE DATA: CONTENT  !!!!!!!
+# # print(response.content)
+
+# soup = bs(response.content, 'html.parser')
+# # print(soup.prettify)
+
+# ul_lists = soup.find_all('ul')
+# # print(ul_lists)
+
+# city_list = soup.find('ul', {'class': 'height6'})
+# print(city_list)
+
+# city_dict = {}
+# for city in city_list.find_all('a'): # all <a> tags
+#     city_dict[city.text] = city['href']
+
+# for k,v in city_dict.items():
+#     print(k,v)
